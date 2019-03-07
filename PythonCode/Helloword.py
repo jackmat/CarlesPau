60d42c025613bb2c6ce3aec50c0809af81af87a6
jackmat <csansfuentesgmail.com>
2019-03-07 11:46:11 +0100

    functions multiplica, suma, resta, square

---
+++ PythonCode/Helloword.py
@@ -0,0 +1,17 @@
+Hello = "Hello World"
+print(Hello)
+
+def square(num):
+	'it returns the num squared'
+	return num**2
+def suma(*num): 
+	return sum(num)
+
+def resta(num1,num2)
+	return(num1-num2)
+def multiplica(num1, num2)
+	return(num1*num2)
+print(square(2))
+print(suma(3,4))
+print(resta(5,1))
+print(multiplica(4,3)
